d9282835cbfa84d6a832f334ce65388f7f5e1c83
Artem Parshyn <parshyn4@gmail.com>
2024-10-26 02:33:04 +0300

    templates, models

--- shop/api/models.py
+++ shop/api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+class Shoes(models.Model):
+    name = models.CharField(max_length=64)
+    category = models.CharField(max_length=64)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    discount = models.IntegerField()
+    material = models.CharField(max_length=64)
+    Lining = models.CharField(max_length=64)
+    Insole = models.CharField(max_length=64)
+    Sole = models.CharField(max_length=64)
